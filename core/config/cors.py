54a60b780525a64e3412cab1c62d8ecfa060e957
ahmedhamila.stage <csahmed17@gmail.com>
2025-11-06 22:06:48 +0100

    Update cors.py

--- core/config/cors.py
+++ core/config/cors.py
@@ -1,12 +1,11 @@
 from core.env import config
 
 
-# CORS_ALLOWED_ORIGINS = [
-#     config("FRONTEND_URL"),
-#     "http://localhost:3000",
-#     "http://127.0.0.1:3000",
-# ]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    config("FRONTEND_URL"),
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 ALLOWED_HOSTS = [
     config("BACKEND_HOST"),
     "127.0.0.1",
